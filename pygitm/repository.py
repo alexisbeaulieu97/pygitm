e9bca550dfe3f38d3515df34bea08f43e738e1aa
Alexis Beaulieu <alexisbeaulieu97@gmail.com>
2022-06-27 16:03:23 -0400

    feat: Added options for git commands

--- pygitm/repository.py
+++ pygitm/repository.py
@@ -1,2 +1,16 @@
 # -*- coding: utf-8 -*-
 
+from pygitm.auth import HTTPSAuth, SSHAuth
+from pygitm.options import CloneOptions
+
+
+class LocalRepository:
+    ...
+
+
+class RemoteRepository:
+    def SSH_clone(self, opts: CloneOptions, auth: SSHAuth) -> LocalRepository:
+        ...  # --config core.sshCommand="ssh -i ~/location/to/private_ssh_key"
+
+    def HTTPS_clone(self, opts: CloneOptions, auth: HTTPSAuth) -> LocalRepository:
+        ...
