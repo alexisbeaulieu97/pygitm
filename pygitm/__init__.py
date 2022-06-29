4d249315e2cbfb0a46d5d423f9c515bcaf418fc7
Alexis Beaulieu <alexisbeaulieu97@gmail.com>
2022-06-29 16:43:20 -0400

    feat: updated API and dependencies

--- pygitm/__init__.py
+++ pygitm/__init__.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from .options import (AddOptions, CheckoutOptions, CloneOptions, CommitOptions,
+                      PullOptions, PushOptions)  # noqa: F401
+
 __version__ = "0.1.0"
---
+++ tests/test_options.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import pygitm
+
+# def test_clone_options_repository():
+#     clone_options = pygitm.CloneOptions("")
+#     opts = clone_options.opts()
+
+#     assert len(opts) == 2
